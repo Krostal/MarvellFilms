b25cab39bd10bc9c3ad664e7aaa43a597dbe9eaa
Krostal <krostal1991@gmail.com>
2023-11-30 19:00:51 +0700

    Initial Commit

---
+++ MarvellFilms/MarvellFilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvellFilmsApp.swift
+//  MarvellFilms
+//
+//  Created by Konstantin Tarasov on 30.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvellFilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
